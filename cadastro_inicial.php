178e45d4685fc91c9552059be2f9f58ed9c1be3b
Fabiano Santos Silva <94010474+kFabada@users.noreply.github.com>
2024-07-28 13:35:22 -0300

    nova tela para cadastro

--- cadastro_inicial.php
+++ cadastro_inicial.php
@@ -1,58 +1,63 @@
 <!DOCTYPE html>
-<html lang="pt_br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style-cadastro.css">
-    <title>Cadastro de Usuario</title>
+    <title>Cadastro Inicial</title>
 </head>
 
 <body>
     <header>
-        <div class="header">
-            <a class="logo" href="./index.php">CyberCoin</a>
-        </div>
+        <a class="logo" href="../cadastro_usuario/index.php">CyberCoin</a>
+        <nav class="container">
+            <!-- <div>
+                <a class="nav-item" href="#">Home</a>
+                <a class="nav-item" href="#">Delivers</a>
+                <a class="nav-item" href="#">Buys</a>
+                <a class="nav-item" href="#">About</a>
+            </div> -->
+        </nav>
     </header>
     <main>
         <section>
-            <div>
-                <form action="processamento/processamento_cadastro.php" method="post">
-                    <div>
-                        <div>
-                            <label for="login">Login</label>
+            <div class="container-form">
+            <h1 class="title-form">Cadastro CyberCoin</h1>
+                <form action="processamento_cadastro.php" method="post">
+                        <div class="form-group">
+                            <div class="form-item">
+                                <div>
+                                    <label for="username">Username</label>
+                                </div>
+                                <input type="text" name="username" id="username" placeholder="username">
+                            </div>
+                            <div class="form-item">
+                                <div>
+                                    <label for="password">Password</label>
+                                </div>
+                                <input type="password" minlength="10" name="password" id="password" placeholder="password">
+                            </div>
+                            <div class="form-item">
+                                <div>
+                                    <label for="email">Email</label>
+                                </div>
+                                <input type="email" name="email" id="email" placeholder="email">
+                            </div>
+                            <div class="form-item">
+                                <div>
+                                    <label for="tel">Telefone</label>
+                                </div>
+                                <input type="text" maxlength="15" name="tel" id="tel" placeholder="telefone">
+                            </div>
+                            <div>
+                                <button id="btn-submit" class="btn-submit" type="submit">Enviar</button>
+                            </div>
                         </div>
-                        <input class="container-item" id="login" name="login" type="text" required>
-                    </div>
-                    <div>
-                        <div>
-                            <label for="senha">Senha</label>
-                        </div>
-                        <input class="container-item" id="senha" name="senha" type="password" minlength="8" required>
-                    </div>
-                    <div>
-                        <div>
-                            <label for="email">Email</label>
-                        </div>
-                        <input class="container-item" id="email" name="email" type="email" required>
-                    </div>
-                    <div>
-                        <div>
-                            <label for="telefone">Telefone</label>
-                        </div>
-                        <input class="container-item" id="telefone" name="telefone" type="tel" required>
-                    </div>
-                    <div>
-                        <button class="btn-submit" type="submit">Enviar Cadastro</button>
-                    </div>
+                </form>
             </div>
-            </form>
-            </div>
+        </section>
     </main>
-    </section>
-    <script>
-
-    </script>
 </body>
 
 </html>
