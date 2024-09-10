3da0ff0102bd9c407f8dab2aeecbb392d4299058
Fabiano Santos Silva <94010474+kFabada@users.noreply.github.com>
2024-09-10 00:38:21 -0300

    metas

--- src/Helpers/info.php
+++ src/Helpers/info.php
@@ -1,9 +1,14 @@
 <?php
-// colocar se ja existe o email no momento do cadastro. 
-// validacao de login e senha. 
-// funcao para fazer funcionar buscarUsuario.
-// fazer tela para alterar_senha.php com apenas o 
-// campo email e busca no banco e se existe o email manda um codigo
-// random para a confirmacao de troca de senha ou apenas um link_random com nome do usuario para a troca.
 
-phpinfo();
+// 1 - Muda todas as telas usando bootstrap
+// 2 - Criar tela de esquece senha
+// 3 - Adicionar o ajax e jquery para fazer requisicao async
+// 4 - Adicionar token para esquece senha e validar usuario mandando mensagem para o email com um link
+// com confirmação ou usando sms
+// 5 - Criar tela do usuario principal depois do login
+// 6 - Talvez Criar um nivel para adm
+// 7 - Verificação de cpf
+// 8 - Adicionar mais campos com dados do usuario e linka tudo isso com o banco e as consultas
+
+
+?>
